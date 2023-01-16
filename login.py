2251776534499d6ad14134cbd22c6f52be55407b
ClementCallendret <callendret.clement@gmail.com>
2023-01-16 13:35:19 +0100

    lala

--- login.py
+++ login.py
@@ -1,2 +1,10 @@
-from flask import request
-#code soon
+from flask import Flask, redirect, url_for, request
+
+@app.route('login',methods = ['POST','GET'])
+def login():
+    if (methods.request == 'POST'):
+        login = request.form['name']
+        password = request.form['password']
+        #Rechercher si login dans base de données
+            #rechercher si password correspond
+                #afficher son compte
