4299548d9d6ee79b849f516d05525178b1dc7d9b
ClementCallendret <callendret.clement@gmail.com>
2023-01-16 13:52:59 +0100

    register+login avec fonction

--- login.py
+++ login.py
@@ -6,5 +6,9 @@ def login():
         login = request.form['name']
         password = request.form['password']
         #Rechercher si login dans base de données
-            #rechercher si password correspond
+        #rechercher si password correspond
+        if (fileIO.lofin.check([login,password])):
                 #afficher son compte
+
+                #supprimer le print quand l'affichage est fait
+                print("It's workingg")
