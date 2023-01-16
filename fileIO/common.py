b1b615f7a760446aef023c08b3e1040ebde4575b
3nd3rh3o <theenderman3333@gmail.com>
2023-01-16 14:44:15 +0100

    fileIO_commonv0.1

--- fileIO/common.py
+++ fileIO/common.py
@@ -1,3 +1,10 @@
+import os.path
+
+if os.path.exists('.\static\save.txt'):
+    print("init save file")
+    with open('.\static\save.txt', 'w') as out:
+        print("\n\n\n==========\n\n\n", file=out)
+
 """ 
 
 def load:
