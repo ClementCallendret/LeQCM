d498046f6bbd392679253ae11fdce2a23463be0d
ClementCallendret <callendret.clement@gmail.com>
2023-01-20 15:13:06 +0100

    registerbugréloslu

--- creation.py
+++ creation.py
@@ -17,6 +17,7 @@ def creation():
         res = []
         for ID in listeQ:
            res.append(fileIO.format.questionToDic(fileIO.question.read(ID)))
+        print(res)
         #res = [{'id' : 1, 'title' : 'titre', 'state' : 'Si A = B Samy est bg', 'answers':[{'val' : True, 'text' : 'je pense que A = B'},{'val' : False, 'text' : 'je pense que A != B'}], 'tags' : ['beau', 'moche sa mère']},
         #    {'id' : 2, 'title' : 'titre2', 'state' : 'Si A = B Samy est moche sa mère', 'answers':[{'val' : False, 'text' : 'je pense que A != B2'},{'val' : True, 'text' : 'je pense que A = B2'}],'tags' : ['moche sa mère', 'moche de fou']}
         #    ]
