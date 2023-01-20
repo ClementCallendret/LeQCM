a8d4f817e4a8052102db4b64e562a37b135df0e9
Samsamy534 <99737503+Samsamy534@users.noreply.github.com>
2023-01-20 15:41:28 +0100

    de la comm

--- formatage.py
+++ formatage.py
@@ -1,8 +1,8 @@
 import markdown
 import pygments
 
-# la variable text correspondra à ce que l'on récupérera de la méthode POST provenant du cod html.
-# OBJECTIF : renvoyer le contenu de cette variable au code html une fois bien traduite avec mermaid en l'occurence. 
+# la variable text correspondra à ce que l'on récupérera de la méthode POST provenant du code html (la on le prof a tapé le code)
+# OBJECTIF : renvoyer le contenu de cette variable au code html une fois bien traduite peu importe si le prof a tapé du mermaid, du code, du LaTeX, etc...
 def formatageMD(text):
     text = '\n'+text.replace('\r', '')
     html = markdown.markdown(text, extensions=['codehilite','fenced_code','md_mermaid'])
