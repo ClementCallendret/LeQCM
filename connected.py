0f7123806989188f4b8e9ca5a9d67ed34a4962bd
ClementCallendret <callendret.clement@gmail.com>
2023-01-21 17:40:04 +0100

    profil clean

--- connected.py
+++ connected.py
@@ -2,8 +2,9 @@ from flask import Blueprint, request,session, render_template
 
 connect = Blueprint('connected',__name__)
 
-@connect.route('/connected/<login>')
-def connected(login):
+@connect.route('/connected/')
+def connected():
+    login = session['login']
     return render_template("connected.html", login=login)
 
 def isConnected():
