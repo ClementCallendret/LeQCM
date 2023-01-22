1f528b970fe7ff2982de85556700334cacad9974
Elio <jon6na@yahoo.com>
2023-01-22 23:09:23 +0100

    Editor UI again

--- formatage.py
+++ formatage.py
@@ -4,9 +4,8 @@ import pygments # NE PAS ENLEVER (même si marqué inutilisé)
 # la variable text correspondra à ce que l'on récupérera de la méthode POST provenant du code html (la on le prof a tapé le code)
 # OBJECTIF : renvoyer le contenu de cette variable au code html une fois bien traduite peu importe si le prof a tapé du mermaid, du code, du LaTeX, etc...
 def formatageMD(text):
-    text2 = text.copy()
-    text2 = '\n'+text2.replace('\r', '')
-    html = markdown.markdown(text2, extensions=['codehilite','fenced_code','md_mermaid'])
+    text = '\n'+text.replace('\r', '')
+    html = markdown.markdown(text, extensions=['codehilite','fenced_code','md_mermaid'])
     return html
 
 def dictTodictFormated(dico):
