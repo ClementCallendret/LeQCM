ac30afdff1aa4258f658a1aae2a170d04bb4ccc6
Clem <82177962+ClementCallendret@users.noreply.github.com>
2023-02-02 14:52:40 +0100

    MARCHE PAS JE SAIS

--- encryption.py
+++ encryption.py
@@ -1,11 +1,12 @@
 import secrets
 import string
 import bcrypt
+import database
 #Alors, oui on est pas en https, oui on est sur python, oui on utilise flask dinc c'est absolument pas sécurisé
 #Mais j'aime la cryptographie, donc on aura une BDD bien sécurisée (j'espère)
 
 #On encrypte
-def poivre(password):
+def pepper(password):
     #conversion du password en bytes
     password = bytes(password, encoding='utf-8')
     #initialisation poivre avec b devant comme ça c'est en byte
@@ -14,54 +15,82 @@ def poivre(password):
     nbChiffre = 2
     for i in range(nbChiffre):
         #on prend des string digits comme ça 10 possibilités (a mutliplié par le nb de tour de boucle)
-        pepper += ''.join(secrets.choice(string.digits))
+        pepper = pepper + (bytes(secrets.choice(string.digits),encoding='utf-8'))
     #on concatène le poivre avec notre password en clair
     passwordP = password + pepper
     return passwordP
 
     
 
-def sel(passwordP):
-    passwordP = bytes(passwordP, encoding='utf-8')
+def salt(passwordP):
+    print("PASSWORDP: ", passwordP)
     #génération du sel
     salt = bcrypt.gensalt()
+
+    #génération du hash
+    passHash = bcrypt.hashpw(passwordP, salt)
+    tab = [passHash,salt]
+    return tab
+
+
+def encrypt(password):
+    #on poivre
+    password = pepper(password)
+    #on sel
+    tab = salt(password)
+    #on return le password salé + poivré et le sel
+    return tab
+
+
+
+#on sel avec le sel correspondant
+def unsalt(passwword,salt):
+    passwordP = bytes(password, encoding='utf-8')
 
     #génération du hash
     passHash = bcrypt.hashpw(passwordP, salt)
     return passHash
 
 
-def encrypt(password):
-    #on poivre
-    password = poivre(password)
-    #on sel
-    password = sel(password)
-    return password
 
-#On décrypte
-def checkPass(login,password):
-
-    #Idem pour le sel
-    #bdd2.get.sel(login)
+#On poivre avec toutes les possibilités
+def unpepper(password, salt):
     listHashPotentiels = []
     hash = b""
     #car 100 possibiltés de poivrage
     for i in range(100):
         #On y passe en bytes avant d'enregistrer 
-        hash = bytes(password+i)
+        print("PASSWORDP: ", password)
+
+        passwordP = password + bytes(i)
+
         #on sale les 100 potentiels hash
-        hash = (sel(hash))
-        listHashPotentiels.append(hash, encoding='utf-8')
+        passHash = unsalt(passwordP,salt)
+        listHashPotentiels.append(passHash, encoding='utf-8')
+
+#On décrypte (STATUT = S SI STUDENT OU P SI PROFESSOR)
+def decrypt(login,password,statut):
+    #On récupère le sel de l'utilisateur
+    salt = b""
+    if (statut == 'P'):
+        salt = database.getProfessorSel(login)
+    elif (statut == 'S'):
+        salt = database.getStudentSel(login)
+
+    listHashPotentiels = unpepper(password, salt)
     
     #on vérifie si le hash de la bdd correpond a un des hash potentiels
     #si oui, c'est le bon mot de passe à l'origine
 
     #On va dans la base de donnée récupéré le hash du mdp avec son login
-
-    #if (bdd.get.hash(login)) in listHashPotentiels:
-    if (True):
-        return True
-    else:
-        return False
+    if (statut == P):
+        for i in range(100):
+            if (databse.matchProfessorPassword(login, listHashPotentiels[i])):
+                return True
+    elif (statut == S):
+        for i in range(100):
+            if (database.matchStudentPassword(login, listHashPotentiels[i])):
+                return True
+    return False
 
     
