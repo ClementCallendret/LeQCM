08bbaa456b15e02d0987fd64057368256507f45f
Samsamy534 <99737503+Samsamy534@users.noreply.github.com>
2023-02-23 16:20:16 +0100

    csv

--- AjoutStudents.py
+++ AjoutStudents.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, request,session, render_template
+import database
+import json
 
 ajt_Stu = Blueprint('AjoutStudents',__name__)
 
 @ajt_Stu.route('/AjoutStudents/')
+def init():
+    return render_template("AjoutStudents.html")
+
+    
+@ajt_Stu.route('/AjoutStudents/', methods = ['POST'])
 def AjoutStudents():
+    if (request.method == 'POST'):
+        studentToAdd = json.loads(request.form["StudentTab"])
+        print(studentToAdd)
+        for e in studentToAdd:
+            database.addStudent(e[0], e[1], e[2], 0)
+
     return render_template("AjoutStudents.html")
 
