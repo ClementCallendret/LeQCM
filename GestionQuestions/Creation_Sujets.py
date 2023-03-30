0a42cfe58774dd0a8511af2739152c960ce62bb9
Samsamy534 <99737503+Samsamy534@users.noreply.github.com>
2023-03-30 11:29:46 +0200

    Page création de sujets

---
+++ GestionQuestions/Creation_Sujets.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request,session, render_template, redirect, url_for, flash
+import database
+import json
+
+crea_sujets = Blueprint('Création_Sujets',__name__)
+
+@crea_sujets.route('/Création_Sujets/')
+def init():
+    if "loginP" in session:
+        return render_template("Création_Sujets.html")
+    else:
+        flash("Vous devez être connecté pour acceder à cette page")
+        return redirect(url_for('login.initRedirect', redirection="Création_Sujets"))
