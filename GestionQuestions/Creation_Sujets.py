c6e4c7a05de74877b1eca0d6616c27576ba16850
ClementCallendret <callendret.clement@gmail.com>
2023-03-31 12:51:01 +0200

    samy

--- GestionQuestions/Creation_Sujets.py
+++ GestionQuestions/Creation_Sujets.py
@@ -7,6 +7,7 @@ crea_sujets = Blueprint('Création_Sujets',__name__)
 @crea_sujets.route('/Création_Sujets/')
 def init():
     if "loginP" in session:
+        tags = database.allTags()
         return render_template("Création_Sujets.html", title="", tags=tags, state="", stateFormated="", idAnswers="[]", answers=[], answersFormated=[], numeralAnswer=0, selectedTag=[], newTags="[]"))
     else:
         flash("Vous devez être connecté pour acceder à cette page")
