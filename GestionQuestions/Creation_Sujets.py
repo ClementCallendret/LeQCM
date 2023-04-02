b4564eb61e1ebdd298abe5108a26d95e457cc7b2
Elio <jon6na@yahoo.com>
2023-04-02 15:55:45 +0200

    pourC

--- GestionQuestions/Creation_Sujets.py
+++ GestionQuestions/Creation_Sujets.py
@@ -9,9 +9,10 @@ crea_sujets = Blueprint('Création_Sujets',__name__)
 @crea_sujets.route('/Création_Sujets/')
 def init():
     if "loginP" in session:
-        tags = database.allTags()
+        tags = database.nbQuestionsPerTag(session["loginP"])
+        profStudents = database.getStudentsByProf(session["loginP"])
         #print(generateurS.tagMultiples([[1,"a"],[2,"b"]]))
-        return render_template("Création_Sujets.html", title="", tags = tags, tagsJSON=json.dumps(tags, indent=4), state="", stateFormated="", idAnswers="[]", answers=[], answersFormated=[], numeralAnswer=0, selectedTag=[], newTags="[]")
+        return render_template("Création_Sujets.html", title="", tags = tags, tagsJSON=json.dumps(tags, indent=4), students=profStudents)
     else:
         flash("Vous devez être connecté pour acceder à cette page")
         return redirect(url_for('login.initRedirect', redirection="Création_Sujets"))
